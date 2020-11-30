6d13df81d088c9733de9642408878ffe696fff12
Jakkha23 <jakob.khaytan@skola.botkyrka.se>
2020-11-30 15:28:13 +0100

    solved includes, indexOf and statistics

---
+++ loop-warmup.js
@@ -0,0 +1,19 @@
+// "While" loop
+let temperature = 20
+
+while(temperature < 25) {
+    console.log(temperature + '°')
+    temperature++
+}
+
+console.log(`the water is boiling, the temperature is now ${temperature}° degrees!`)
+
+// "For" loop
+
+i = 1
+
+for (i = 1; i < 20; ++i) {
+    console.log(i)
+}
+
+console.log(`'i' is now ${i}`)
--- statistics.js
+++ statistics.js
@@ -1,12 +1,38 @@
+console.log("-----------------------------------------------------------------------------")
+let highestNumber = 0
 
 function max(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > highestNumber) {
+            highestNumber = arr[i]
+        }
+    }
+    return "Högsta stygghet: " + highestNumber
 }
+
+let smallestnumber = 10000
 
 function min(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < smallestnumber) {
+            smallestnumber = i
+        }
+    }
+    return 'Lägsta stygghet: ' + smallestnumber
 }
 
+let total = 0
 function mean(arr) {
-    // Ledtråd: Börja med att räkna ut summan av alla element
+    for (let i = 0; i < arr.length; i++) {
+        total = total + arr[i]
+    }
+    console.log("-")
+    console.log("total stygghet: " + total)
+    console.log("personer: " + arr.length)
+    return 'Medel stygghet: ' + (total / arr.length)
 }
+
+console.log(max([2, 0, 5, 8, 4, 5, 9]))
+console.log(min([2, 0, 5, 8, 4, 5, 9]))
+console.log(mean([2, 0, 5, 8, 4, 5, 9]))
+console.log("-----------------------------------------------------------------------------")
